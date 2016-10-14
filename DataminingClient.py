e1d2bd27bde24d5c8cfa45549bbf0b64d642798a
Aaron Peters <100483612@oncampus.local>
2016-10-14 18:27:59 -0400

    Added timer to the main program and added some small additions to the readme

--- DataminingClient.py
+++ DataminingClient.py
@@ -2,6 +2,8 @@
 import tweepy
 import json
 import pymongo
+import time
+import sys
 from pymongo import MongoClient
 client = MongoClient()
 db = client.TwitterData
@@ -13,16 +15,19 @@ access_token_secret = '5RXedN8QkTIrvHeTpZtTDNtvPWt9qrB4EmS1wDnTNx6ko'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+start = time.time()
 
 def writeToDb(dbnum, result):
 	db.altcoin.insert_one({"Date":result["created_at"],"Id":result["id_str"],"Text":result["text"]})
-
+	
 class StreamListener(tweepy.StreamListener):
 
 	def on_status(self, status):
 		print(status.text)
 		
 	def on_data(self, data):
+		global add
+		global minute
 		result = json.loads(data)
 		id = result["id_str"]
 		created = result["created_at"]
@@ -35,11 +40,20 @@ class StreamListener(tweepy.StreamListener):
 		if "appl" in text.lower(): db.appl.insert_one({"Date":result["created_at"],"Id":result["id_str"],"Text":result["text"]})
 		if "goog" in text.lower(): db.goog.insert_one({"Date":result["created_at"],"Id":result["id_str"],"Text":result["text"]})
 		if "yhoo" in text.lower(): db.yhoo.insert_one({"Date":result["created_at"],"Id":result["id_str"],"Text":result["text"]})
+		curtime = int(time.time()-start)
+		if (curtime % 60) == 1: add = True
+		if (curtime % 60) == 0 and add: 
+			add=False
+			minute+=1
+			print(minute)
+
 
 		
 twitterStreamListener = StreamListener()
 twitterStream = tweepy.Stream(auth = api.auth, listener=StreamListener())
-
+minute = 0
+add=True
+print "Started program"
 twitterStream.filter(track=['altcoin,bitcoin,coindesk,cryptocurrency,gold,APPL,GOOG,YHOO'])
 api.update_status('Test tweet for datamining')
 
